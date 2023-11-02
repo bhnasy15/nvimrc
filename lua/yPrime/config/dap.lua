652d8b5b68870ec5e4eac0a134efbb718faca9db
yarob <yarob.021@gmail.com>
2023-11-02 12:51:13 +0200

    Thu Nov  2 12:51:31 PM EET 2023

--- lua/yPrime/config/dap.lua
+++ lua/yPrime/config/dap.lua
@@ -15,35 +15,27 @@ dap.defaults.fallback.external_terminal = {
 
 dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
 dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
-dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+dap.listeners.before.event_exited["dapui_config"]     = function() dapui.close() end
 
-vim.keymap.set('n', '<space>dc', function() dap.continue() end)
-vim.keymap.set('n', '<space>do>', function() dap.step_over() end)
-vim.keymap.set('n', '<space>dI', function() dap.step_into() end)
-vim.keymap.set('n', '<space>dO', function() dap.step_out() end)
-vim.keymap.set('n', '<space>db', function() dap.toggle_breakpoint() end)
-vim.keymap.set('n', '<space>dB', function() dap.set_breakpoint() end)
-vim.keymap.set('n', '<space>dlp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-vim.keymap.set('n', '<space>dr', function() dap.repl.open() end)
-vim.keymap.set('n', '<space>dR', function() dap.run_last() end)
-vim.keymap.set({'n', 'v'}, '<space>dK', function()
-	dap_widget.hover()
-end)
-vim.keymap.set({'n', 'v'}, '<space>dp', function()
-	dap_widget.preview()
-end)
-vim.keymap.set('n', '<space>df', function()
-	dap_widget.centered_float(widgets.frames)
-end)
-vim.keymap.set('n', '<space>ds', function()
-	dap_widget.centered_float(widgets.scopes)
-end)
+vim.keymap.set('n'       , '<space>dc' , function() dap.continue() end)
+vim.keymap.set('n'       , '<space>do>', function() dap.step_over() end)
+vim.keymap.set('n'       , '<space>dI' , function() dap.step_into() end)
+vim.keymap.set('n'       , '<space>dO' , function() dap.step_out() end)
+vim.keymap.set('n'       , '<space>db' , function() dap.toggle_breakpoint() end)
+vim.keymap.set('n'       , '<space>dB' , function() dap.set_breakpoint() end)
+vim.keymap.set('n'       , '<space>dlp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n'       , '<space>dr' , function() dap.repl.open() end)
+vim.keymap.set('n'       , '<space>dR' , function() dap.run_last() end)
+vim.keymap.set({'n', 'v'}, '<space>dK' , function() dap_widget.hover() end)
+vim.keymap.set({'n', 'v'}, '<space>dp' , function() dap_widget.preview() end)
+vim.keymap.set('n'       , '<space>df' , function() dap_widget.centered_float(widgets.frames) end)
+vim.keymap.set('n'       , '<space>ds' , function() dap_widget.centered_float(widgets.scopes) end)
 
-vim.keymap.set('n', '<space>duo', function() dapui.open() end)
-vim.keymap.set('n', '<space>duc', function() dapui.close() end)
-vim.keymap.set('n', '<space>dut', function() dapui.toggle() end)
-vim.keymap.set('n', '<space>duu', function() dapui.float_element() end)
-vim.keymap.set({'v', 'n'}, '<space>de', function () dapui.eval() end)
+vim.keymap.set('n'       , '<space>duo', function() dapui.open() end)
+vim.keymap.set('n'       , '<space>duc', function() dapui.close() end)
+vim.keymap.set('n'       , '<space>dut', function() dapui.toggle() end)
+vim.keymap.set('n'       , '<space>duu', function() dapui.float_element() end)
+vim.keymap.set({'v', 'n'}, '<space>de' , function () dapui.eval() end)
 
 dap.adapters.coreclr = {
 	type = 'executable',
