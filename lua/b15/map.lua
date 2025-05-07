d8aa2a4b029a65b0929263742cda08c664129f37
bhnasy15 <yarob.021@gmail.com>
2025-05-07 11:21:04 +0300

    Wed May  7 11:21:11 EEST 2025

--- lua/b15/map.lua
+++ lua/b15/map.lua
@@ -10,3 +10,7 @@ vim.keymap.set('n', '<leader>oo', vim.cmd.Explore, {silent=true})
 
 vim.keymap.set('n', '<leader>tt', function() vim.cmd('tabnew +term') end, {silent=true})
 
+vim.keymap.set('n', '<leader>|', function()
+    local cmd = vim.fn.input("$ ")
+    vim.cmd.cexpr({'system("' .. cmd .. '")'})
+end, {silent=true})
